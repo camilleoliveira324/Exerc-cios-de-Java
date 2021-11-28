a57ce6dab2f6bfb71f1e4ee60637fd885593360d
camioliveira24 <84456300+camioliveira24@users.noreply.github.com>
2021-11-28 16:42:36 -0300

    .

--- Exercicios/src/DiaDaSemana.java
+++ Exercicios/src/DiaDaSemana.java
@@ -26,5 +26,16 @@ public class DiaDaSemana {
         } else if (day == 7) {
             System.out.println("Você escolheu o Sábado");
         }
+
+        switch (diaSemana) {
+            case 1: System.out.println("Você escolheu o Domingo"); break;
+            case 2: System.out.println("Você escolheu a Segunda"); break;
+            case 3: System.out.println("Você escolheu a Terça"); break;
+            case 4: System.out.println("Você escolheu a Quarta"); break;
+            case 5: System.out.println("Você escolheu a Quinta"); break;
+            case 6: System.out.println("Você escolheu a Sexta, bom fim de semana"); break;
+            case 7: System.out.println("Você escolheu o Sábado"); break;
+            default: System.out.println("Você não digitou um número válido")
+        }
     }
 }
